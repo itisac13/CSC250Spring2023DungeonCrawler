962508d18fb80dcaf6626e4c4bc6b877d923b8c4
itisac13 <129095529+itisac13@users.noreply.github.com>
2023-05-01 10:28:53 -0500

    music

--- Dungeon Crawler/Assets/Scripts/Fight Scripts/FightController.cs
+++ Dungeon Crawler/Assets/Scripts/Fight Scripts/FightController.cs
@@ -8,11 +8,23 @@ public class FightController : MonoBehaviour
 {
     private Player player;
     private Monster monster;
+    public GameObject playerobj;
+    public GameObject monsterobj;
     public Text stats;
     public Text attackCommentary;
 
+    public AudioSource victoryMusic;
+    public AudioSource defeatMusic;
+
     private bool yourTurn;
     private bool gameEnded;
+
+    private bool playingMusic;
+
+
+    private Vector3 playerOriginalPosition;
+    private Vector3 monsterOriginalPosition;
+    
     
 
     private string[] availableNames = { "Bob", "Howard", "Steve", "Donald", "Stewart" };
@@ -22,6 +34,10 @@ public class FightController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        this.playerOriginalPosition = this.playerobj.transform.position;
+        this.monsterOriginalPosition = this.monsterobj.transform.position;
+
+        this.playingMusic= false;
         this.yourTurn = true;
         this.gameEnded = false;
         this.player = MasterData.player;
@@ -37,25 +53,55 @@ public class FightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (this.yourTurn)
         {
-            SceneManager.LoadScene("DungeonRoom");
+            this.monsterobj.transform.position = this.monsterOriginalPosition;
+            this.playerobj.transform.position = new Vector3(0.0f, 0.5f, 0.0f);
+        }
+        else
+        {
+            this.playerobj.transform.position = this.playerOriginalPosition;
+            this.monsterobj.transform.position = new Vector3(0.0f, 0.5f, 0.0f);
         }
         if (this.player.getHitpoints() <= 0)
         {
-            this.attackCommentary.text = "YOU LOSE!!!! LOL!!!!\n\npress space to exit";
-            this.gameEnded = true;
+            Renderer renderer = this.playerobj.GetComponent<Renderer>();
+            
+            renderer.enabled = false;
+            this.attackCommentary.text = "YOU LOSE!!!! LOL!!!!\n\npress space to restart the game";
+            // this.gameEnded = true;
+            // Destroy(this.playerobj);
 
             // restart the game
+            this.gameEnded = true;
+
+
             MasterData.player = new Player("PLAYER");
             MasterData.dungeon = new Dungeon(100, MasterData.player);
             MasterData.room = MasterData.player.getCurrentRoom();
             MasterData.lastExit = string.Empty;
+
+            if (!this.playingMusic)
+            {
+                this.playingMusic = true;
+                this.defeatMusic.Play();
+            }
         }
         if (this.monster.getHitpoints() <= 0)
         {
-            this.attackCommentary.text = "you win\n\nPress space to exit";
+            
+
+            Renderer renderer = this.monsterobj.GetComponent<Renderer>();
+            renderer.enabled = false;
+            this.attackCommentary.text = "you win\n\nPress space to continue";
             this.gameEnded = true;
+            // Destroy(this.monsterobj);
+            if (!this.playingMusic)
+            {
+                this.playingMusic = true;
+                this.victoryMusic.Play();
+            }
+            
         }
         this.stats.text = "";
         string playerStats = this.player.getData();
@@ -73,23 +119,33 @@ public class FightController : MonoBehaviour
 
         
         
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            if (this.yourTurn)
+        if (!this.gameEnded) {
+            if (Input.GetKeyDown(KeyCode.A))
             {
-                this.yourTurn = false;
-                bool result = Fight.Attack(this.player, this.monster);
-                
-                
+                if (this.yourTurn)
+                {
+                    this.yourTurn = false;
+                    bool result = Fight.Attack(this.player, this.monster);
+
+
+                }
+
             }
-            
+            if (!this.yourTurn)
+            {
+                this.yourTurn = true;
+                bool result = Fight.Attack(this.monster, this.player);
+
+            }
         }
-        if (!this.yourTurn)
+        else
         {
-            this.yourTurn = true;
-            bool result = Fight.Attack(this.monster, this.player);
-            
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("DungeonRoom");
+            }
         }
+        
 
     }
 
